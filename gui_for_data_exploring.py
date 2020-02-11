15e47e3f3a7141bdf96ad317f88c3794ed34eedd
billingsley-john <johnbillingsley1996@yahoo.com>
2020-02-11 11:56:21 +0000

    added kernel density replacements for all histograms

--- gui_for_data_exploring.py
+++ gui_for_data_exploring.py
@@ -32,27 +32,27 @@ def load_data():
 
 @st.cache
 def find_type_of_entries_in_each_field():
-        catorgorical_key_names = []
-        continious_key_names = []
+        categorical_key_names = []
+        continuous_key_names = []
 
         for entry in data.keys(): 
                 if entry not in ['tbr_error', 'number_of_batches', 'particles_per_batch']:
                         if type(data[entry][0]) == str:
-                                catorgorical_key_names.append(entry)
+                                categorical_key_names.append(entry)
                         else:
-                                continious_key_names.append(entry)
-        return catorgorical_key_names, continious_key_names
+                                continuous_key_names.append(entry)
+        return categorical_key_names, continuous_key_names
 
-def plot_graphs():
-        figs = []
-        for catorgory in catorgorical_key_names:
+def plot_histogram():
+        # figs = []
+        for category in categorical_key_names:
                 fig = go.Figure()
-                for material in selected_catorgorical_key_names[catorgory]:
+                for material in selected_categorical_key_names[category]:
 
-                        print(catorgory, material)
+                        print(category, material)
 
 
-                        y_data = filtered_data[(filtered_data[catorgory] == material)]['tbr']
+                        y_data = filtered_data[(filtered_data[category] == material)]['tbr']
 
                         fig.add_trace(go.Histogram(x=y_data,
                                                 name= material,
@@ -68,24 +68,62 @@ def plot_graphs():
                 fig.update_layout(barmode='overlay',
                                 xaxis_title="TBR",
                                 yaxis_title="number of simulations",
-                                title="Impact of "+catorgory.replace('_',' ')+ " on TBR",
-                                showlegend=True
+                                title="Impact of "+category.replace('_',' ')+ " on TBR",
+                                showlegend=True,                                
                                 )
 
                 fig.update_traces(opacity=0.4)
-                figs.append(fig)
+                # figs.append(fig)
+                st.write(fig)
+
+def calculate_bandwidth(data):
+        # only uses first 100 values to calculate bandwidth
+        bandwidths = 10 ** np.linspace(-1, 1, 100)
+        grid = GridSearchCV(KernelDensity(kernel='gaussian'),
+                            {'bandwidth': bandwidths},
+                            cv=LeaveOneOut())
+        grid.fit(data.head(100)[:, None])
+        approx_bandwidth = grid.best_params_['bandwidth']
+        return approx_bandwidth
+
+def plot_kernel_density():
+        # figs = []
+        for category in categorical_key_names:
+                fig = go.Figure()
+                for material in selected_categorical_key_names[category]:
+
+                        data = filtered_data[(filtered_data[category] == material)]['tbr']
+
+                        x_d = np.linspace(min(data), max(data), 1000)
+                        kde = KernelDensity(bandwidth=calculate_bandwidth(data), kernel='gaussian')
+                        kde.fit(data[:, None])
+                        logprob = kde.score_samples(x_d[:, None])
+
+                        fig.add_trace(go.Scatter(x=x_d,
+                                                 y=np.exp(logprob),
+                                                 name=material,
+                        ))
+
+                fig.update_layout(xaxis_title="TBR",
+                                  yaxis_title="Number of simulations",
+                                  title="Impact of "+category.replace('_', ' ')+ " on TBR",
+                                  showlegend=True,
+                                  )
+
+                fig.update_traces(opacity=0.5)
+                # figs.append(fig)
                 st.write(fig)
 
 def perform_data_filtering(filtered_data):
         filters = []
-        for key, value in selected_continious_values.items():
+        for key, value in selected_continuous_values.items():
                 print('key',key)
                 # key_name = entry.key()
                 print('value',value)
                 # data[]
                 filtered_data = filtered_data[(filtered_data[key]>=value[0])&(filtered_data[key]<=value[1])]
 
-        for key, value in selected_catorgorical_key_names.items():
+        for key, value in selected_categorical_key_names.items():
                 print('key',key)
                 # key_name = entry.key()
                 print('value',value)
@@ -100,23 +138,23 @@ def perform_data_filtering(filtered_data):
 
 
 def write_multiselect_boxes():
-        selected_catorgorical_key_names = {}
+        selected_categorical_key_names = {}
 
-        for key_name in catorgorical_key_names:
+        for key_name in categorical_key_names:
 
                 defaults = list(data[key_name].unique())
 
-                selected_catorgorical_key_names[key_name] = st.multiselect(
+                selected_categorical_key_names[key_name] = st.multiselect(
                                                                         label = key_name.replace('_',' '), 
                                                                         default=list(defaults),
                                                                         options=list(data[key_name].unique())
                                                                         )
-        return selected_catorgorical_key_names
+        return selected_categorical_key_names
 
 
 def write_slider_bars():
-        selected_continious_values = {}
-        for key_name in continious_key_names:
+        selected_continuous_values = {}
+        for key_name in continuous_key_names:
 
                         cleaned_key_name = key_name.replace('_', ' ')
 
@@ -126,42 +164,16 @@ def write_slider_bars():
                         if min_val != max_val:
                                 selected = st.slider('select a range of ' + str(cleaned_key_name), min_val, max_val, (min_val, max_val))
 
-                                selected_continious_values[key_name] = selected
-        return selected_continious_values
+                                selected_continuous_values[key_name] = selected
+        return selected_continuous_values
 
 def draw_model_diagram():
-        if selected_catorgorical_key_names['model'] != None:
+        if selected_categorical_key_names['model'] != None:
 
-                if len(selected_catorgorical_key_names['model']) == 1:
-                        st.image('./images/'+selected_catorgorical_key_names['model'][0]+'.png')
-                if len(selected_catorgorical_key_names['model']) == 2:
+                if len(selected_categorical_key_names['model']) == 1:
+                        st.image('./images/'+selected_categorical_key_names['model'][0]+'.png')
+                if len(selected_categorical_key_names['model']) == 2:
                         st.image('./images/both.png')
-
-
-
-def calculate_approximate_bandwidth(data):
-        # only uses first 100 values to calculate bandwidth
-        bandwidths = 10 ** np.linspace(-1, 1, 100)
-        grid = GridSearchCV(KernelDensity(kernel='gaussian'),
-                            {'bandwidth': bandwidths},
-                            cv=LeaveOneOut())
-        grid.fit(data.head(100)[:, None])
-        approx_bandwidth = grid.best_params_['bandwidth']
-        return approx_bandwidth
-
-def plot_kde_graph(data):
-        x_d = np.linspace(0, 2, 1000)   # change to max(tbr) for upper limit
-
-        kde = KernelDensity(bandwidth=calculate_approximate_bandwidth(data), kernel='gaussian')
-        kde.fit(data[:, None])
-        logprob = kde.score_samples(x_d[:, None])
-
-        plt.plot(x_d, np.exp(logprob), alpha=0.5)
-        # plt.fill_between(x_d, np.exp(logprob), alpha=0.5)
-        # plt.plot(data, np.full_like(data, -0.01), '|k', markeredgewidth=1)
-        plt.ylim(0.00, 2.00)
-        st.pyplot(plt)
-
 
 
 
@@ -170,23 +182,21 @@ data = load_data()
 st.title('TBR results explorer')
 st.text('Filter a dataset of '+str(len(data))+ ' simulations')
 
-catorgorical_key_names, continious_key_names = find_type_of_entries_in_each_field()
+categorical_key_names, continuous_key_names = find_type_of_entries_in_each_field()
 
 
-selected_catorgorical_key_names = write_multiselect_boxes()
+selected_categorical_key_names = write_multiselect_boxes()
 
 draw_model_diagram()
 
-selected_continious_values = write_slider_bars()
+selected_continuous_values = write_slider_bars()
 
 filtered_data = perform_data_filtering(data)
 
 st.write(filtered_data)
 
+if st.button("Plot histogram"):
+        plot_histogram()
 
-# plots a basic kde using all of the filtered tbr data
-# this will need to be incorporated into the plot_graphs() function
-plot_kde_graph(filtered_data['tbr'])
-
-
-# plot_graphs()
+if st.button("Plot Kernel Density"):
+        plot_kernel_density()
